e7dc1b10a424c282930161c7154482b4aacade76
liyake04@gmail.com <liyake04@gmail.com@4ebeba31-cbc1-aedc-ca5e-33a6bebab04a>
2012-11-17 14:34:39 +0000

    1、完善行情走势图的绘制。 2、增加了行情走势图的排序功能。
    欠缺：
    1、行情走势图的分类
    2、行情走势图的操作功能（翻页、移动列等）

--- trunk/MarketTrendWidget.cpp
+++ trunk/MarketTrendWidget.cpp
@@ -1,14 +1,23 @@
 #include "StdAfx.h"
 #include "MarketTrendWidget.h"
+#include "DataEngine.h"
+
+#define	GetColorByFloat(x)	(((x)==0.0) ? QColor(191,191,191) : (((x)>0.0) ? QColor(255,80,80) : QColor(0,255,255)))
+
 
 CMarketTrendWidget::CMarketTrendWidget( CBaseWidget* parent /*= 0*/ )
 	: CBaseWidget(parent,CBaseWidget::MarketTrend)
-	, m_iHeaderHeight(16)
-	, m_iItemHeight(16)
+	, m_iHeaderHeight(20)
+	, m_iStockHeight(16)
 	, m_iBottomHeight(20)
 	, m_rtHeader(0,0,0,0)
 	, m_rtClient(0,0,0,0)
 	, m_rtBottom(0,0,0,0)
+	, showHeaderIndex(0)
+	, showStockIndex(0)
+	, m_pSelectedStock(0)
+	, m_iSortColumn(0)
+	, m_sortOrder(Qt::AscendingOrder)
 {
 	m_listHeader << tr("索引") << tr("代码") << tr("名称")
 		<< tr("涨幅") << tr("量比") << tr("换手率（仓差）") << tr("前收") << tr("今开")
@@ -17,19 +26,135 @@ CMarketTrendWidget::CMarketTrendWidget( CBaseWidget* parent /*= 0*/ )
 		<< tr("流通市值") << tr("外盘") << tr("内盘") << tr("委买量") << tr("委卖量")
 		<< tr("委比") << tr("委差");
 
+	/*初始化各个单元的宽度*/
+	m_listItemWidth.push_back(30);
+	for(int i=0;i<m_listHeader.size()-1;++i)
+	{
+		m_listItemWidth.push_back(80);
+	}
+	m_listItemWidth[5] = 100;
+
 	m_pMenuCustom = new QMenu(tr("市场行情图菜单"));
+
+	setStocks(CDataEngine::getDataEngine()->getStockInfoList());
+	m_pSelectedStock = m_listStocks.first();
 }
 
 CMarketTrendWidget::~CMarketTrendWidget(void)
 {
+	clearTmpData();
+	delete m_pMenuCustom;
 }
 
-void CMarketTrendWidget::paintEvent( QPaintEvent* e )
+void CMarketTrendWidget::setStocks( const QList<CStockInfoItem*>& list )
+{
+	clearTmpData();
+	m_listStocks = list;
+	for(int i=0;i<m_listStocks.size();++i)
+	{
+		m_mapStockIndex[m_listStocks[i]] = i;
+	}
+	foreach(CStockInfoItem* pItem,m_listStocks)
+	{
+		connect(pItem,SIGNAL(stockInfoItemChanged(const QString&)),this,SLOT(stockInfoChanged(const QString&)));
+	}
+}
+
+void CMarketTrendWidget::stockInfoChanged( const QString& code )
+{
+	CStockInfoItem* pItem = CDataEngine::getDataEngine()->getStockInfoItem(code);
+	update(rectOfStock(pItem));
+}
+
+void CMarketTrendWidget::clearTmpData()
+{
+	m_pSelectedStock = 0;
+	foreach(CStockInfoItem* pItem,m_listStocks)
+	{
+		disconnect(pItem,SIGNAL(stockInfoItemChanged(const QString&)),this,SLOT(stockInfoChanged(const QString&)));
+	}
+	m_listStocks.clear();
+	m_mapStockIndex.clear();
+}
+
+void CMarketTrendWidget::clickedHeader( int column )
+{
+	if(column==0)
+		return;
+	if(column == m_iSortColumn)
+	{
+		m_sortOrder = (m_sortOrder==Qt::AscendingOrder) ? Qt::DescendingOrder : Qt::AscendingOrder;
+	}
+	else
+	{
+		m_iSortColumn = column;
+		m_sortOrder = Qt::AscendingOrder;
+	}
+	if(column == 1||column ==2)
+	{
+		QMultiMap<QString,CStockInfoItem*> mapSort;
+		foreach(CStockInfoItem* pItem,m_listStocks)
+		{
+			mapSort.insert(dataOfDisplay(pItem,column),pItem);
+		}
+		
+		if(m_sortOrder==Qt::AscendingOrder)
+			setStocks(mapSort.values());
+		else
+		{
+			QList<CStockInfoItem*> list;
+			QMultiMap<QString,CStockInfoItem*>::iterator iter = mapSort.begin();
+			while(iter!=mapSort.end())
+			{
+				list.push_front(iter.value());
+				++iter;
+			}
+			setStocks(list);
+		}
+	}
+	else
+	{
+		QMultiMap<float,CStockInfoItem*> mapSort;
+		foreach(CStockInfoItem* pItem,m_listStocks)
+		{
+			QString qsData = dataOfDisplay(pItem,column);
+			qsData = qsData.replace("%","");
+			mapSort.insert(qsData.toFloat(),pItem);
+		}
+		if(m_sortOrder==Qt::AscendingOrder)
+			setStocks(mapSort.values());
+		else
+		{
+			QList<CStockInfoItem*> list;
+			QMultiMap<float,CStockInfoItem*>::iterator iter = mapSort.begin();
+			while(iter!=mapSort.end())
+			{
+				list.push_front(iter.value());
+				++iter;
+			}
+			setStocks(list);
+		}
+	}
+	update(m_rtHeader);
+	update(m_rtClient);
+}
+
+void CMarketTrendWidget::clickedStock( CStockInfoItem* pItem )
+{
+	CStockInfoItem* pPreSelectedStock = m_pSelectedStock;
+	m_pSelectedStock = pItem;
+	update(rectOfStock(pPreSelectedStock));
+	update(rectOfStock(m_pSelectedStock));
+}
+
+void CMarketTrendWidget::paintEvent( QPaintEvent* )
 {
 	QPainter p(this);
 	drawHeaders(p);
-	p.fillRect(m_rtClient,QColor(255,255,0));
-	p.fillRect(m_rtBottom,QColor(255,255,255));
+	drawStocks(p);
+	p.fillRect(m_rtBottom,QColor(0,255,0));
+	p.setPen(QColor(255,255,255));
+	p.drawRect(m_rtBottom);
 }
 
 void CMarketTrendWidget::resizeEvent( QResizeEvent* e)
@@ -38,8 +163,58 @@ void CMarketTrendWidget::resizeEvent( QResizeEvent* e)
 	return CBaseWidget::resizeEvent(e);
 }
 
+void CMarketTrendWidget::mousePressEvent( QMouseEvent* e )
+{
+	e->accept();
+	QPoint ptCur = e->pos();
+	if(m_rtHeader.contains(ptCur))
+	{
+		int iCurX = m_rtHeader.left();
+		int iCurIndex = 0;
+		for (;iCurIndex<3;++iCurIndex)
+		{
+			iCurX = iCurX+m_listItemWidth[iCurIndex];
+			if(iCurX>ptCur.x())
+				break;
+		}
+		if(iCurX>ptCur.x())
+		{
+			clickedHeader(iCurIndex);
+			return;
+		}
+		iCurIndex = iCurIndex+showHeaderIndex;
+		for (;iCurIndex<m_listItemWidth.size();++iCurIndex)
+		{
+			iCurX = iCurX+m_listItemWidth[iCurIndex];
+			if(iCurX>ptCur.x())
+				break;
+		}
+		if(iCurX>ptCur.x())
+		{
+			clickedHeader(iCurIndex);
+			return;
+		}
+	}
+	else if(m_rtClient.contains(ptCur))
+	{
+		int iCurIndex = showStockIndex+(ptCur.y()-m_rtClient.top())/m_iStockHeight;
+		if(iCurIndex>=0&&iCurIndex<m_listStocks.size())
+		{
+			clickedStock(m_listStocks[iCurIndex]);
+		}
+	}
+	else if(m_rtBottom.contains(ptCur))
+	{
+
+	}
+}
+
 QMenu* CMarketTrendWidget::getCustomMenu()
 {
+	QAction* pAction = m_pMenu->menuAction();
+	if(!m_pMenuCustom->actionGeometry(pAction).isValid())
+		m_pMenuCustom->addMenu(m_pMenu);
+
 	return m_pMenuCustom;
 }
 
@@ -48,10 +223,402 @@ void CMarketTrendWidget::updateDrawRect()
 	QRect rtClient = this->rect();
 	m_rtHeader = QRect(rtClient.topLeft(),QSize(rtClient.width(),m_iHeaderHeight));
 	m_rtBottom = QRect(rtClient.left(),rtClient.bottom()-m_iBottomHeight,rtClient.width(),m_iBottomHeight);
-	m_rtClient = QRect(m_rtHeader.bottomLeft(),QSize(rtClient.width(),rtClient.height()-m_iBottomHeight-m_iHeaderHeight));
+	m_rtClient = QRect(m_rtHeader.left(),m_rtHeader.bottom()+2,rtClient.width(),rtClient.height()-m_iBottomHeight-m_iHeaderHeight-2);
+}
+
+QString CMarketTrendWidget::dataOfDisplay( CStockInfoItem* itemData,int column )
+{
+	switch(column)
+	{
+	case 0:
+		{
+			//索引号
+			if(m_mapStockIndex.contains(itemData))
+				return QString("%1").arg(m_mapStockIndex[itemData]+1);
+			return QString("Error");
+		}
+		break;
+	case 1:
+		{
+			//股票代码
+			return itemData->getCode();
+		}
+		break;
+	case 2:
+		{
+			//股票名称
+			return itemData->getName();
+		}
+		break;
+	case 3:
+		{
+			//涨幅
+			if(_isnan(itemData->getIncrease()))
+				return QString();
+			return QString("%1%").arg(itemData->getIncrease(),0,'f',2);
+		}
+		break;
+	case 4:
+		{
+			//量比
+			if(_isnan(itemData->getVolumeRatio()))
+				return QString();
+			return QString("%1").arg(itemData->getVolumeRatio(),0,'f',2);
+		}
+		break;
+	case 5:
+		{
+			//换手率
+			if(_isnan(itemData->getTurnRatio()))
+				return QString();
+			return QString("%1%").arg(itemData->getTurnRatio(),0,'f',2);
+		}
+		break;
+	case 6:
+		{
+			//前收
+			if(_isnan(itemData->getLastClose()))
+				return QString();
+
+			return QString("%1").arg(itemData->getLastClose(),0,'f',2);
+		}
+		break;
+	case 7:
+		{
+			//今开
+			if(_isnan(itemData->getOpenPrice()))
+				return QString();
+
+			return QString("%1").arg(itemData->getOpenPrice(),0,'f',2);
+		}
+		break;
+	case 8:
+		{
+			//最高
+			if(_isnan(itemData->getHighPrice()))
+				return QString();
+
+			return QString("%1").arg(itemData->getHighPrice(),0,'f',2);
+		}
+		break;
+	case 9:
+		{
+			//最低
+			if(_isnan(itemData->getLowPrice()))
+				return QString();
+
+			return QString("%1").arg(itemData->getLowPrice(),0,'f',2);
+		}
+		break;
+	case 10:
+		{
+			//最新
+			if(_isnan(itemData->getNewPrice()))
+				return QString();
+
+			return QString("%1").arg(itemData->getNewPrice(),0,'f',2);
+		}
+		break;
+	case 11:
+		{
+			//总手
+			if(_isnan(itemData->getTotalVolume()))
+				return QString();
+
+			return QString("%1").arg(itemData->getTotalVolume(),0,'f',0);
+		}
+		break;
+	case 12:
+		{
+			//总额（持仓）
+			if(_isnan(itemData->getTotalAmount()))
+				return QString();
+
+			return QString("%1").arg(itemData->getTotalAmount()/10000,0,'f',0);
+		}
+		break;
+	case 13:
+		{
+			//现手
+			if(_isnan(itemData->getNowVolume()))
+				return QString();
+
+			return QString("%1").arg(itemData->getNowVolume(),0,'f',0);
+		}
+		break;
+	case 14:
+		{
+			//涨速
+			if(_isnan(itemData->getIncSpeed()))
+				return QString();
+
+			return QString("%1%").arg(itemData->getIncSpeed()*100,0,'f',2);
+		}
+		break;
+	case 15:
+		{
+			//涨跌
+			if(_isnan(itemData->getPriceFluctuate()))
+				return QString();
+
+			return QString("%1%").arg(itemData->getPriceFluctuate(),0,'f',2);
+		}
+		break;
+	case 16:
+		{
+			//振幅
+			if(_isnan(itemData->getAmplitude()))
+				return QString();
+
+			return QString("%1%").arg(itemData->getAmplitude()*100,0,'f',2);
+		}
+		break;
+	case 17:
+		{
+			//均价
+			if(_isnan(itemData->getAvePrice()))
+				return QString();
+
+			return QString("%1").arg(itemData->getAvePrice(),0,'f',2);
+		}
+		break;
+	case 18:
+		{
+			//市盈率
+			if(_isnan(itemData->getPERatio()))
+				return QString();
+			return QString("%1").arg(itemData->getPERatio(),0,'f',2);
+		}
+		break;
+	case 19:
+		{
+			//流通市值
+			if(_isnan(itemData->getLTSZ()))
+				return QString();
+			return QString("%1").arg(itemData->getLTSZ(),0,'f',0);
+		}
+		break;
+	case 20:
+		{
+			//外盘
+			return static_cast<int>(itemData->getSellVOL());
+		}
+		break;
+	case 21:
+		{
+			//内盘
+			return static_cast<int>(itemData->getBuyVOL());
+		}
+		break;
+	case 22:
+		{
+			//委买量
+			if(_isnan(itemData->getBIDVOL()))
+				return QString();
+
+			return QString("%1").arg(itemData->getBIDVOL(),0,'f',0);
+		}
+		break;
+	case 23:
+		{
+			//委卖量
+			if(_isnan(itemData->getASKVOL()))
+				return QString();
+
+			return QString("%1").arg(itemData->getASKVOL(),0,'f',0);
+		}
+		break;
+	case 24:
+		{
+			//委比
+			if(_isnan(itemData->getCommRatio()))
+				return QString();
+
+			return QString("%1%").arg(itemData->getCommRatio(),0,'f',2);
+		}
+		break;
+	case 25:
+		{
+			//委差
+			if(_isnan(itemData->getCommSent()))
+				return QString();
+
+			return QString("%1").arg(itemData->getCommSent(),0,'f',0);
+		}
+		break;
+	default:
+		{
+			return QString("NULL");
+		}
+		break;
+	}
+}
+
+QColor CMarketTrendWidget::dataOfColor( CStockInfoItem* itemData,int column )
+{
+	if(column == 1)
+		return QColor(127,255,191);
+	else if(column == 2)
+		return QColor(255,255,0);
+	else if(column == 3 || column == 15)
+	{
+		//涨幅
+		return GetColorByFloat(itemData->getIncrease());
+	}
+	else if(column == 7)
+	{
+		//今开
+		float f = itemData->getOpenPrice()-itemData->getLastClose();
+		return GetColorByFloat(f);
+	}
+	else if(column == 8)
+	{
+		//最高
+		float f = itemData->getHighPrice()-itemData->getLastClose();
+		return GetColorByFloat(f);
+	}
+	else if(column == 9)
+	{
+		//最低
+		float f = itemData->getLowPrice()-itemData->getLastClose();
+		return GetColorByFloat(f);
+	}
+	else if(column == 10)
+	{
+		float f = itemData->getNewPrice()-itemData->getLastClose();
+		return GetColorByFloat(f);
+	}
+	else if(column == 17)
+	{
+		float f = itemData->getAvePrice()-itemData->getLastClose();
+		return GetColorByFloat(f);
+	}
+	else if(column == 20 || column == 22)
+		return QColor(255,80,80);
+	else if(column == 21 || column == 23)
+		return QColor(0,255,255);
+	else if(column == 24)
+	{
+		float f = itemData->getCommRatio();
+		return GetColorByFloat(f);
+	}
+	else if(column == 25)
+	{
+		float f = itemData->getCommSent();
+		return GetColorByFloat(f);
+	}
+
+	return QColor(191,191,191);
+}
+
+QRect CMarketTrendWidget::rectOfStock( CStockInfoItem* pItem )
+{
+	if(m_mapStockIndex.contains(pItem))
+	{
+		int iRow = m_mapStockIndex[pItem];
+		return QRect(m_rtClient.left(),(m_rtClient.top()+(iRow-showStockIndex)*m_iStockHeight),m_rtClient.width(),m_iStockHeight);
+	}
+
+	return QRect();
 }
 
 void CMarketTrendWidget::drawHeaders( QPainter& p )
 {
-	p.fillRect(m_rtHeader,QColor(255,0,0));
+	p.setPen(QColor(255,255,255));
+	p.fillRect(m_rtHeader,QColor(0,0,0));
+	int iCurX = m_rtHeader.left();
+	int iCurIndex = 0;
+	for (;iCurIndex<3;++iCurIndex)
+	{
+		QRect rtItem = QRect(iCurX,m_rtHeader.top(),m_listItemWidth[iCurIndex],m_rtHeader.height());
+		p.drawRect(rtItem);
+		if((m_iSortColumn == iCurIndex)&&iCurIndex!=0)
+		{
+			if(m_sortOrder == Qt::AscendingOrder)
+			{
+				p.drawText(rtItem,Qt::AlignCenter,QString("↑%1").arg(m_listHeader[iCurIndex]));
+			}
+			else
+			{
+				p.drawText(rtItem,Qt::AlignCenter,QString("↓%1").arg(m_listHeader[iCurIndex]));
+			}
+		}
+		else
+		{
+			p.drawText(rtItem,Qt::AlignCenter,m_listHeader[iCurIndex]);
+		}
+		iCurX = iCurX+m_listItemWidth[iCurIndex];
+	}
+
+	iCurIndex = iCurIndex+showHeaderIndex;
+	while(iCurX<m_rtHeader.right()&&iCurIndex<m_listHeader.size())
+	{
+		QRect rtItem = QRect(iCurX,m_rtHeader.top(),m_listItemWidth[iCurIndex],m_rtHeader.height());
+		p.drawRect(rtItem);
+		if((m_iSortColumn == iCurIndex))
+		{
+			if(m_sortOrder == Qt::AscendingOrder)
+			{
+				p.drawText(rtItem,Qt::AlignCenter,QString("↑%1").arg(m_listHeader[iCurIndex]));
+			}
+			else
+			{
+				p.drawText(rtItem,Qt::AlignCenter,QString("↓%1").arg(m_listHeader[iCurIndex]));
+			}
+		}
+		else
+		{
+			p.drawText(rtItem,Qt::AlignCenter,m_listHeader[iCurIndex]);
+		}
+		iCurX = iCurX+m_listItemWidth[iCurIndex];
+		++iCurIndex;
+	}
+}
+
+void CMarketTrendWidget::drawStocks( QPainter& p )
+{
+	p.fillRect(m_rtClient,QColor(0,0,0));
+	int iCurRow = showStockIndex;
+	int iCurY = m_rtClient.top();
+	while(iCurY<m_rtClient.bottom()&&iCurRow<m_listStocks.size())
+	{
+		QRect rtStock = QRect(m_rtClient.left(),iCurY,m_rtClient.width(),m_iStockHeight);
+		CStockInfoItem* pStock = m_listStocks[iCurRow];
+
+		if(m_pSelectedStock == pStock)
+		{
+			p.fillRect(rtStock,QColor(50,50,50));
+		}
+		drawStock(p,rtStock,pStock);
+
+		++iCurRow;
+		iCurY = iCurY + m_iStockHeight;
+	}
+}
+
+void CMarketTrendWidget::drawStock( QPainter& p,const QRect& rtStock,CStockInfoItem* pItem )
+{
+	int iCurX = rtStock.left();
+	int iCurIndex = 0;
+	for (;iCurIndex<3;++iCurIndex)
+	{
+		QRect rtItem = QRect(iCurX,rtStock.top(),m_listItemWidth[iCurIndex],rtStock.height());
+		p.setPen(QColor(127,0,0));
+		p.drawRect(rtItem);
+		p.setPen(dataOfColor(pItem,iCurIndex));
+		p.drawText(rtItem,Qt::AlignCenter,dataOfDisplay(pItem,iCurIndex));
+		iCurX = iCurX+m_listItemWidth[iCurIndex];
+	}
+
+	iCurIndex = iCurIndex+showHeaderIndex;
+	while(iCurX<m_rtHeader.right()&&iCurIndex<m_listHeader.size())
+	{
+		QRect rtItem = QRect(iCurX,rtStock.top(),m_listItemWidth[iCurIndex],rtStock.height());
+		p.setPen(QColor(127,0,0));
+		p.drawRect(rtItem);
+		p.setPen(dataOfColor(pItem,iCurIndex));
+		p.drawText(rtItem,Qt::AlignCenter,dataOfDisplay(pItem,iCurIndex));
+		iCurX = iCurX+m_listItemWidth[iCurIndex];
+		++iCurIndex;
+	}
 }
--- trunk/MarketTrendWidget.h
+++ trunk/MarketTrendWidget.h
@@ -8,17 +8,35 @@
 #ifndef MARKET_TREND_WIDGET_H
 #define	MARKET_TREND_WIDGET_H
 #include "BaseWidget.h"
+#include "StockInfoItem.h"
 
 class CMarketTrendWidget : public CBaseWidget
 {
 	Q_OBJECT
+	//struct stHeaderInfo
+	//{
+	//	QString text;
+	//	int column;
+	//};
 public:
 	CMarketTrendWidget(CBaseWidget* parent = 0);
 	~CMarketTrendWidget(void);
 
+public:
+	void setStocks(const QList<CStockInfoItem*>& list);		//设置要显示的股票列表
+
+public slots:
+	void stockInfoChanged(const QString& code);				//某只股票的信息发生改变
+
+private:
+	void clearTmpData();						//清理本窗口中创建的内存。
+	void clickedHeader(int column);				//当点击头部时触发
+	void clickedStock(CStockInfoItem* pItem);	//当点击股票时触发
+
 protected:
 	virtual void paintEvent(QPaintEvent* e);				//绘制事件
 	virtual void resizeEvent(QResizeEvent* e);				//大小改变的事件
+	virtual void mousePressEvent(QMouseEvent* e);			//鼠标点击事件
 
 	//虚函数，各个控件的自定义菜单。
 	virtual QMenu* getCustomMenu();
@@ -26,11 +44,25 @@ protected:
 private:
 	QMenu* m_pMenuCustom;					//自定义菜单
 	QStringList m_listHeader;				//头部数据
+	QList<CStockInfoItem*> m_listStocks;	//要显示的股票列表
+	QMap<CStockInfoItem*,int> m_mapStockIndex;	//用来快速查找某只股票所在的索引
+	CStockInfoItem* m_pSelectedStock;		//当前选中的股票
+	int m_iSortColumn;						//当前进行排序的列
+	Qt::SortOrder m_sortOrder;				//当前排序方式
 
 	/*用于绘制的成员函数*/
 private:
 	void updateDrawRect();					//更新绘制区域的大小
-	void drawHeaders(QPainter& p);						//绘制头部信息
+	void drawHeaders(QPainter& p);			//绘制头部信息
+	void drawStocks(QPainter& p);			//绘制股票信息
+	void drawStock(QPainter& p,const QRect& rtStock,CStockInfoItem* pItem);	//绘制单个股票
+
+	//判断鼠标点击的位置，做出相应的响应
+	//void hitHeader(QPoint& ptCur);
+
+	QString dataOfDisplay(CStockInfoItem* itemData,int column);
+	QColor dataOfColor(CStockInfoItem* itemData,int column);
+	QRect rectOfStock(CStockInfoItem* pItem);			//获取某只股票显示的位置
 
 	/*用于绘制的成员变量*/
 private:
@@ -39,8 +71,12 @@ private:
 	QRect m_rtBottom;						//底部区域，用于绘制分类，鼠标操作等信息
 
 	int m_iHeaderHeight;					//头部的高度
-	int m_iItemHeight;						//单个行情块的高度
+	int m_iStockHeight;						//单个行情块的高度
 	int m_iBottomHeight;					//底部的高度
+
+	int showHeaderIndex;					//开始显示的头部索引-横向（0,1,2,5即为5-3=2）
+	int showStockIndex;						//开始显示的股票索引-纵向
+	QList<int> m_listItemWidth;				//各个item的宽度。
 };
 
 
