d1068f158da902efa473f036de1e9d50bd635a1c
liyake04@gmail.com <liyake04@gmail.com@4ebeba31-cbc1-aedc-ca5e-33a6bebab04a>
2012-12-12 09:56:07 +0000

    完善色块图的功能。

--- trunk/ColorBlockWidget.cpp
+++ trunk/ColorBlockWidget.cpp
@@ -7,12 +7,14 @@ CColorBlockWidget::CColorBlockWidget( CBaseWidget* parent /*= 0*/ )
 	, m_pMenuCustom(0)
 	, m_iCBHeight(16)
 	, showStockIndex(0)
+	, m_pSelectedStock(0)
 {
 	m_pMenuCustom = new QMenu(tr("色块图菜单"));
 }
 
 CColorBlockWidget::~CColorBlockWidget(void)
 {
+	clearTmpData();
 	delete m_pMenuCustom;
 }
 
@@ -48,20 +50,26 @@ bool CColorBlockWidget::savePanelInfo( QDomDocument& doc,QDomElement& eleWidget 
 
 void CColorBlockWidget::setBlock( const QString& block )
 {
-	m_listStocks.clear();
-	
-	disconnect(this,SLOT(updateStock(const QString&)));
+	clearTmpData();
 
 	m_listStocks = CDataEngine::getDataEngine()->getStocksByBlock(block);
-
-	foreach(CStockInfoItem* pItem,m_listStocks)
+	showStockIndex = 0;
+	for(int i=0;i<m_listStocks.size();++i)
 	{
+		CStockInfoItem* pItem = m_listStocks[i];
+
+		m_mapStockIndex[pItem] = i;
 		//建立更新机制
 		connect(pItem,SIGNAL(stockItemHistoryChanged(const QString&)),this,SLOT(updateStock(const QString&)));
 		connect(pItem,SIGNAL(stockItemMinuteChanged(const QString&)),this,SLOT(updateStock(const QString&)));
 	}
 
 	m_qsBlock = block;
+
+	if(m_listStocks.size()>0)
+	{
+		clickedStock(m_listStocks.first());
+	}
 
 	update();
 	return CBaseWidget::setBlock(block);
@@ -72,13 +80,153 @@ void CColorBlockWidget::updateStock( const QString& code )
 	
 }
 
+void CColorBlockWidget::clearTmpData()
+{
+	m_pSelectedStock = 0;
+	m_listStocks.clear();
+	m_mapStockIndex.clear();
+
+	disconnect(this,SLOT(updateStock(const QString&)));
+}
+
+void CColorBlockWidget::clickedStock( CStockInfoItem* pItem )
+{
+	if(pItem == m_pSelectedStock)
+		return;
+	int iShowCount = m_rtClient.height()/m_iCBHeight;
+	int iRow = m_mapStockIndex[pItem];
+	if((iRow<showStockIndex)||(iRow>showStockIndex+iShowCount))
+	{
+		showStockIndex = iRow;
+		update(m_rtClient);
+	}
+
+	CStockInfoItem* pPreSelectedStock = m_pSelectedStock;
+	m_pSelectedStock = pItem;
+	update(rectOfStock(pPreSelectedStock));
+	update(rectOfStock(m_pSelectedStock));
+}
+
 void CColorBlockWidget::paintEvent( QPaintEvent* )
 {
 	QPainter p(this);
 	QRect rtClient = this->rect();
-	drawHeader(p,QRect(rtClient.left(),rtClient.top(),rtClient.width(),20));
-	drawClient(p,QRect(rtClient.left(),rtClient.top()+21,rtClient.width(),rtClient.height()-40));
-	drawBottom(p,QRect(rtClient.left(),rtClient.bottom()-20,rtClient.width(),20));
+	m_rtHeader = QRect(rtClient.left(),rtClient.top(),rtClient.width(),20);
+	m_rtClient = QRect(rtClient.left(),rtClient.top()+21,rtClient.width(),rtClient.height()-40);
+	m_rtBottom = QRect(rtClient.left(),rtClient.bottom()-20,rtClient.width(),20);
+
+	drawHeader(p,m_rtHeader);
+	drawClient(p,m_rtClient);
+	drawBottom(p,m_rtBottom);
+}
+
+void CColorBlockWidget::mousePressEvent( QMouseEvent* e )
+{
+	e->accept();
+	QPoint ptCur = e->pos();
+	if(m_rtHeader.contains(ptCur))
+	{
+
+	}
+	else if(m_rtClient.contains(ptCur))
+	{
+		int iCurIndex = showStockIndex+(ptCur.y()-m_rtClient.top())/m_iCBHeight;
+		if(iCurIndex>=0&&iCurIndex<m_listStocks.size())
+		{
+			clickedStock(m_listStocks[iCurIndex]);
+		}
+	}
+	else if(m_rtBottom.contains(ptCur))
+	{
+
+	}
+}
+
+void CColorBlockWidget::wheelEvent( QWheelEvent* e )
+{
+	int numDegrees = e->delta() / 8;
+	int numSteps = numDegrees / 15;
+	int iIndex = showStockIndex-numSteps*5;
+	if(iIndex>=0&&iIndex<m_listStocks.size())
+	{
+		e->accept();
+		showStockIndex = iIndex;
+		update();
+	}
+	return CBaseWidget::wheelEvent(e);
+}
+
+void CColorBlockWidget::keyPressEvent( QKeyEvent* e )
+{
+	if(Qt::Key_Left == e->key())
+	{
+		e->accept();
+		return;
+	}
+	else if(Qt::Key_Right == e->key())
+	{
+		e->accept();
+		return;
+	}
+	else if(Qt::Key_Down == e->key())
+	{
+		int iCurIndex = m_mapStockIndex[m_pSelectedStock];
+		if(m_listStocks.size()>(iCurIndex+1))
+		{
+			CStockInfoItem* pItem = m_listStocks[iCurIndex+1];
+			int iRow = m_mapStockIndex[pItem];
+			int iShowCount = m_rtClient.height()/m_iCBHeight;
+			if(iShowCount<1)
+				return;
+			if((iRow-showStockIndex)>=iShowCount)
+			{
+				showStockIndex = iRow-iShowCount+1;
+				update(m_rtClient);
+			}
+			clickedStock(m_listStocks[iCurIndex+1]);
+		}
+		e->accept();
+	}
+	else if(Qt::Key_Up == e->key())
+	{
+		int iCurIndex = m_mapStockIndex[m_pSelectedStock];
+		if(iCurIndex>0)
+		{
+			CStockInfoItem* pItem = m_listStocks[iCurIndex-1];
+			int iRow = m_mapStockIndex[pItem];
+			if(iRow<showStockIndex)
+			{
+				showStockIndex = iRow;
+				update(m_rtClient);
+			}
+			clickedStock(pItem);
+		}
+		e->accept();
+	}
+	else if(Qt::Key_PageDown == e->key())
+	{
+		int iShowCount = m_rtClient.height()/m_iCBHeight;
+		if(iShowCount<1)
+			return;
+		if((showStockIndex+iShowCount)<m_listStocks.size())
+		{
+			showStockIndex = showStockIndex+iShowCount;
+			update(m_rtClient);
+		}
+		e->accept();
+	}
+	else if(Qt::Key_PageUp == e->key())
+	{
+		int iShowCount = m_rtClient.height()/m_iCBHeight;
+		if(iShowCount<1)
+			return;
+		showStockIndex = (showStockIndex-iShowCount)>0 ? (showStockIndex-iShowCount) : 0;
+		update(m_rtClient);
+
+		e->accept();
+	}
+
+	return CBaseWidget::keyPressEvent(e);
 }
 
 QMenu* CColorBlockWidget::getCustomMenu()
@@ -123,6 +271,10 @@ void CColorBlockWidget::drawBottom( QPainter& p,const QRect& rtBottom )
 
 void CColorBlockWidget::drawStock( QPainter& p,const QRect& rtCB,CStockInfoItem* pItem )
 {
+	if(pItem == m_pSelectedStock)
+	{
+		p.fillRect(rtCB,QColor(20,20,20));
+	}
 	int iCBCount = rtCB.width()/m_iCBHeight;
 	QList<qRcvHistoryData*> list = pItem->getLastHistory(iCBCount+1);
 
@@ -148,3 +300,15 @@ void CColorBlockWidget::drawStock( QPainter& p,const QRect& rtCB,CStockInfoItem*
 		iCurX = iCurX+m_iCBHeight;
 	}
 }
+
+QRect CColorBlockWidget::rectOfStock( CStockInfoItem* pItem )
+{
+	if(m_mapStockIndex.contains(pItem))
+	{
+		int iRow = m_mapStockIndex[pItem];
+		return QRect(m_rtClient.left(),(m_rtClient.top()+(iRow-showStockIndex)*m_iCBHeight),m_rtClient.width(),m_iCBHeight);
+	}
+
+	return QRect();
+}
+
