f9e640c29f6093303ca934df8fd5936da41fb2b6
liyake04 <liyake04@gmail.com>
2013-01-07 14:11:13 +0000

    增加板块的色块图窗口

---
+++ BColorBlockWidget.cpp
@@ -0,0 +1,13 @@
+#include "StdAfx.h"
+#include "BColorBlockWidget.h"
+
+
+CBColorBlockWidget::CBColorBlockWidget(CBaseWidget* parent /*= 0*/ )
+	: CCoordXBaseWidget(parent,CBaseWidget::ColorBlock)
+{
+}
+
+
+CBColorBlockWidget::~CBColorBlockWidget(void)
+{
+}
---
+++ BColorBlockWidget.h
@@ -0,0 +1,14 @@
+#ifndef B_COLOR_BLOCK_WIDGET_H
+#define	B_COLOR_BLOCK_WIDGET_H
+
+#include "CoordXBaseWidget.h"
+
+class CBColorBlockWidget : public CCoordXBaseWidget
+{
+	Q_OBJECT
+public:
+	CBColorBlockWidget(CBaseWidget* parent = 0);
+	~CBColorBlockWidget(void);
+};
+
+#endif // !B_COLOR_BLOCK_WIDGET_H
