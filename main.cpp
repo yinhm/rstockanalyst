f2afc00d823e025541a410f984616f3d92d64c9d
liyake04 <liyake04@gmail.com>
2013-01-08 10:34:23 +0000

    添加Lua的支持

--- main.cpp
+++ main.cpp
@@ -8,8 +8,47 @@
 #include "KLineWidget.h"
 #include "SplashDlg.h"
 
+extern "C"
+{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+};
+
+static int pppp(lua_State* _L)
+{
+	int n = lua_gettop(_L);
+	const char* aaa = lua_tostring(_L,0);
+	lua_getglobal(_L,"QString");
+	QString* p = (QString*)(lua_topointer(_L,-1));
+	qDebug()<<*p;
+	return 1;
+	for (int i=1;i<=n;++i)
+	{
+		int _type = lua_type(_L,i);
+		printf(lua_tostring(_L,i));
+	}
+	return 1;
+}
+
 int main(int argc, char *argv[])
 {
+	lua_CFunction pFun = &pppp;
+	lua_State* L;
+	L = luaL_newstate();
+	QString qsTitle = "test for pointer to lua";
+
+	lua_register(L,"print",pFun);
+	lua_pushlightuserdata(L,&qsTitle);
+	lua_setglobal(L,"QString");
+	luaL_dostring(L,"print(\"Lua worked!!!\\r\\n\")");
+//	luaL_dostring(L,"print(math.abs(-33))");
+	//luaL_error(L,aaa);
+	//qDebug()<<QString::fromLocal8Bit(aaa);
+	lua_close(L);
+	return 0;
+
+
     QApplication app(argc, argv);
 
 	QTextCodec::setCodecForLocale(QTextCodec::codecForName("GB2312"));
