fb939c656e90f4b211efa09ca38f559c0dafc23d
liyake04 <liyake04@gmail.com>
2013-07-23 09:47:57 +0000

    增加5分钟内价格变化情况

--- ColorBlockWidget.h
+++ ColorBlockWidget.h
@@ -33,6 +33,7 @@ public:
 		IndexTurnRatio,			//换手率
 		IndexGuanDan,			//当前挂单情况
 		Index5DayLine,			//最近5日涨幅变化
+		IndexCmpPrice,			//最近5分钟内的价格变化
 	};
 
 
@@ -142,6 +143,8 @@ private:
 private:
 	int m_iTitleHeight;						//头部高度
 	int m_iBottomHeight;					//底部的高度
+	int m_iLeftLen;							//左侧的空闲区域
+	int m_iRightLen;						//右侧的空闲区域
 	int showStockIndex;						//当前显示的起始位置（列）
 
 	QRect m_rtHeader;						//头部Header区域
